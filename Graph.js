9dcd7d5cc95c8982081675ffd1ab4c900fde4f7f
jmfarp2011 <jamesmeador1983@gmail.com>
2014-11-17 16:24:10 +0000

    added filter/sort of child keys

--- Graph.js
+++ Graph.js
@@ -1,109 +1,3 @@
-window.String.prototype.sha1 = window.String.prototype.sha1 || function() {
-  /** Extend String object with method to encode multi-byte string to utf8
-  * - monsur.hossa.in/2012/07/20/utf-8-in-javascript.html */
-  String.prototype.utf8Encode = String.prototype.utf8Encode || function() {
-    return unescape( encodeURIComponent( this ) );
-  };
-  /** Extend String object with method to decode utf8 string to multi-byte */
-  String.prototype.utf8Decode = String.prototype.utf8Decode || function() {
-    try {
-      return decodeURIComponent( escape( this ) );
-    } catch (e) {
-      return this; // invalid UTF-8? return as-is
-    }
-  };
-  
-  /**
-  * Function 'f' [§4.1.1].
-  * @private
-  */
-  var _f = function(s, x, y, z) {
-    switch (s) {
-    case 0: return (x & y) ^ (~x & z); // Ch()
-    case 1: return x ^ y ^ z; // Parity()
-    case 2: return (x & y) ^ (x & z) ^ (y & z); // Maj()
-    case 3: return x ^ y ^ z; // Parity()
-    }
-  };
-  /**
-  * Rotates left (circular left shift) value x by n positions [§3.2.5].
-  * @private
-  */
-  var _ROTL = function(x, n) {
-    return (x<<n) | (x>>>(32-n));
-  };
-  /**
-  * Hexadecimal representation of a number.
-  * @private
-  */
-  var _toHexStr = function(n) {
-    // note can't use toString(16) as it is implementation-dependant,
-    // and in IE returns signed numbers when used on full words
-    var s="", v;
-    for (var i=7; i>=0; i--) { v = (n>>>(i*4)) & 0xf; s += v.toString(16); }
-    return s;
-  };
-  
-  var msg = this.toString();
-  // convert string to UTF-8, as SHA only deals with byte-streams
-  msg = msg.utf8Encode();
-  // constants [§4.2.1]
-  var K = [ 0x5a827999, 0x6ed9eba1, 0x8f1bbcdc, 0xca62c1d6 ];
-  // PREPROCESSING
-  msg += String.fromCharCode(0x80); // add trailing '1' bit (+ 0's padding) to string [§5.1.1]
-  // convert string msg into 512-bit/16-integer blocks arrays of ints [§5.2.1]
-  var l = msg.length/4 + 2; // length (in 32-bit integers) of msg + ‘1’ + appended length
-  var N = Math.ceil(l/16); // number of 16-integer-blocks required to hold 'l' ints
-  var M = new Array(N);
-  for (var i=0; i<N; i++) {
-    M[i] = new Array(16);
-      for (var j=0; j<16; j++) { // encode 4 chars per integer, big-endian encoding
-        M[i][j] = (msg.charCodeAt(i*64+j*4)<<24) | (msg.charCodeAt(i*64+j*4+1)<<16) |
-        (msg.charCodeAt(i*64+j*4+2)<<8) | (msg.charCodeAt(i*64+j*4+3));
-      } // note running off the end of msg is ok 'cos bitwise ops on NaN return 0
-    }
-  // add length (in bits) into final pair of 32-bit integers (big-endian) [§5.1.1]
-  // note: most significant word would be (len-1)*8 >>> 32, but since JS converts
-  // bitwise-op args to 32 bits, we need to simulate this by arithmetic operators
-  M[N-1][14] = ((msg.length-1)*8) / Math.pow(2, 32); M[N-1][14] = Math.floor(M[N-1][14]);
-  M[N-1][15] = ((msg.length-1)*8) & 0xffffffff;
-  // set initial hash value [§5.3.1]
-  var H0 = 0x67452301;
-  var H1 = 0xefcdab89;
-  var H2 = 0x98badcfe;
-  var H3 = 0x10325476;
-  var H4 = 0xc3d2e1f0;
-  // HASH COMPUTATION [§6.1.2]
-  var W = new Array(80); var a, b, c, d, e;
-  for (var i=0; i<N; i++) {
-  // 1 - prepare message schedule 'W'
-  for (var t=0; t<16; t++) W[t] = M[i][t];
-  for (t=16; t<80; t++) W[t] = _ROTL(W[t-3] ^ W[t-8] ^ W[t-14] ^ W[t-16], 1);
-  // 2 - initialise five working variables a, b, c, d, e with previous hash value
-  a = H0; b = H1; c = H2; d = H3; e = H4;
-  // 3 - main loop
-  for (t=0; t<80; t++) {
-    var s = Math.floor(t/20); // seq for blocks of 'f' functions and 'K' constants
-    var T = (_ROTL(a,5) + _f(s,b,c,d) + e + K[s] + W[t]) & 0xffffffff;
-    e = d;
-    d = c;
-    c = _ROTL(b, 30);
-    b = a;
-    a = T;
-    }
-    // 4 - compute the new intermediate hash value (note 'addition modulo 2^32')
-    H0 = (H0+a) & 0xffffffff;
-    H1 = (H1+b) & 0xffffffff;
-    H2 = (H2+c) & 0xffffffff;
-    H3 = (H3+d) & 0xffffffff;
-    H4 = (H4+e) & 0xffffffff;
-  }
-  return _toHexStr(H0) + _toHexStr(H1) + _toHexStr(H2) +
-  _toHexStr(H3) + _toHexStr(H4);
-};
-
-//-------------------------------------------------------------------------------------------------------------------------------//
-
 function Graph(){}
 
 
@@ -132,13 +26,121 @@ Graph['Edge'] = function(edge){
 Graph['Collection'] = function(_parent, items){
   var _indexes = {},
       _data = [],
-      self = this;
+      self = this,
+      _database = _parent instanceof Graph.Database ? _parent : _parent.db();
   Object.defineProperty(this, 'length', {
     get: function(){return _data.length}
   });
   
+  var _sha1 = function(obj){
+    var str = JSON.stringify(obj);
+    /** Extend String object with method to encode multi-byte string to utf8
+    * - monsur.hossa.in/2012/07/20/utf-8-in-javascript.html */
+    var utf8Encode = function(str) {
+      return unescape( encodeURIComponent( str ) );
+    };
+    /** Extend String object with method to decode utf8 string to multi-byte */
+    var utf8Decode = function(str) {
+      try {
+        return decodeURIComponent( escape( str ) );
+      } catch (e) {
+        return str; // invalid UTF-8? return as-is
+      }
+    };
+    
+    /**
+    * Function 'f' [§4.1.1].
+    * @private
+    */
+    var _f = function(s, x, y, z) {
+      switch (s) {
+      case 0: return (x & y) ^ (~x & z); // Ch()
+      case 1: return x ^ y ^ z; // Parity()
+      case 2: return (x & y) ^ (x & z) ^ (y & z); // Maj()
+      case 3: return x ^ y ^ z; // Parity()
+      }
+    };
+    /**
+    * Rotates left (circular left shift) value x by n positions [§3.2.5].
+    * @private
+    */
+    var _ROTL = function(x, n) {
+      return (x<<n) | (x>>>(32-n));
+    };
+    /**
+    * Hexadecimal representation of a number.
+    * @private
+    */
+    var _toHexStr = function(n) {
+      // note can't use toString(16) as it is implementation-dependant,
+      // and in IE returns signed numbers when used on full words
+      var s="", v;
+      for (var i=7; i>=0; i--) { v = (n>>>(i*4)) & 0xf; s += v.toString(16); }
+      return s;
+    };
+    
+    var msg = str.toString();
+    // convert string to UTF-8, as SHA only deals with byte-streams
+    msg = utf8Encode(msg);
+    // constants [§4.2.1]
+    var K = [ 0x5a827999, 0x6ed9eba1, 0x8f1bbcdc, 0xca62c1d6 ];
+    // PREPROCESSING
+    msg += String.fromCharCode(0x80); // add trailing '1' bit (+ 0's padding) to string [§5.1.1]
+    // convert string msg into 512-bit/16-integer blocks arrays of ints [§5.2.1]
+    var l = msg.length/4 + 2; // length (in 32-bit integers) of msg + ‘1’ + appended length
+    var N = Math.ceil(l/16); // number of 16-integer-blocks required to hold 'l' ints
+    var M = new Array(N);
+    for (var i=0; i<N; i++) {
+      M[i] = new Array(16);
+        for (var j=0; j<16; j++) { // encode 4 chars per integer, big-endian encoding
+          M[i][j] = (msg.charCodeAt(i*64+j*4)<<24) | (msg.charCodeAt(i*64+j*4+1)<<16) |
+          (msg.charCodeAt(i*64+j*4+2)<<8) | (msg.charCodeAt(i*64+j*4+3));
+        } // note running off the end of msg is ok 'cos bitwise ops on NaN return 0
+      }
+    // add length (in bits) into final pair of 32-bit integers (big-endian) [§5.1.1]
+    // note: most significant word would be (len-1)*8 >>> 32, but since JS converts
+    // bitwise-op args to 32 bits, we need to simulate this by arithmetic operators
+    M[N-1][14] = ((msg.length-1)*8) / Math.pow(2, 32); M[N-1][14] = Math.floor(M[N-1][14]);
+    M[N-1][15] = ((msg.length-1)*8) & 0xffffffff;
+    // set initial hash value [§5.3.1]
+    var H0 = 0x67452301;
+    var H1 = 0xefcdab89;
+    var H2 = 0x98badcfe;
+    var H3 = 0x10325476;
+    var H4 = 0xc3d2e1f0;
+    // HASH COMPUTATION [§6.1.2]
+    var W = new Array(80); var a, b, c, d, e;
+    for (var i=0; i<N; i++) {
+    // 1 - prepare message schedule 'W'
+    for (var t=0; t<16; t++) W[t] = M[i][t];
+    for (t=16; t<80; t++) W[t] = _ROTL(W[t-3] ^ W[t-8] ^ W[t-14] ^ W[t-16], 1);
+    // 2 - initialise five working variables a, b, c, d, e with previous hash value
+    a = H0; b = H1; c = H2; d = H3; e = H4;
+    // 3 - main loop
+    for (t=0; t<80; t++) {
+      var s = Math.floor(t/20); // seq for blocks of 'f' functions and 'K' constants
+      var T = (_ROTL(a,5) + _f(s,b,c,d) + e + K[s] + W[t]) & 0xffffffff;
+      e = d;
+      d = c;
+      c = _ROTL(b, 30);
+      b = a;
+      a = T;
+      }
+      // 4 - compute the new intermediate hash value (note 'addition modulo 2^32')
+      H0 = (H0+a) & 0xffffffff;
+      H1 = (H1+b) & 0xffffffff;
+      H2 = (H2+c) & 0xffffffff;
+      H3 = (H3+d) & 0xffffffff;
+      H4 = (H4+e) & 0xffffffff;
+    }
+    return _toHexStr(H0) + _toHexStr(H1) + _toHexStr(H2) +
+    _toHexStr(H3) + _toHexStr(H4);
+  };
+  
+  var genIndex = _database.indexGenerator || _sha1;
+  
   var _index = function(obj){
-    obj.cid = obj.cid || JSON.stringify(obj).sha1();
+    obj.cid = obj.id || obj.cid || genIndex(obj);
     return obj.cid;
   };
   
@@ -175,6 +177,14 @@ Graph['Collection'] = function(_parent, items){
     return new Graph.Collection(_parent, _data.slice());
   };
   
+  var _keysFromLevels = function(node, levels){
+    if (levels.length > 1){
+      return _keysFromLevels(node[levels[0]], levels.slice(1));
+    }else{
+      return node[levels[0]];
+    }
+  };
+  
   this.filter = function(filters, exclude){
     exclude = !!exclude;
     var keys = Object.keys(filters),
@@ -187,15 +197,18 @@ Graph['Collection'] = function(_parent, items){
     for (var i = 0, len = filterData.length; i < len; i++){
       for (var j = 0; j < data.length; j++){
         var filter = filterData[i];
-        data[j][filter.key] && (_ops[filter.op](data[j][filter.key], filter.value) === exclude) && data.splice(j--,1);
+        _keysFromLevels(data[j], filter.key) && (_ops[filter.op](_keysFromLevels(data[j], filter.key), filter.value) === exclude) && data.splice(j--,1);
       }
     }
     return new Graph.Collection(_parent, data);
   };
   
   this.sort = function(key, desc){
-    function compare(a,b) {
+    var levels = key.split('__');
+    function compare(first, second) {
       var ret = !desc ? 1 : -1;
+      var a = _keysFromLevels(first, levels),
+        b = _keysFromLevels(second, levels);
       if (a[key] < b[key])
          return -1*ret;
       if (a[key] > b[key])
@@ -247,6 +260,7 @@ Graph['Entity'] = function(entity, database){
     }
   }
   
+  this.db = function(){ return database; };
   
   this.on = function(event, callback){
       _listeners[event] = callback;
@@ -371,7 +385,6 @@ Graph['Entity'] = function(entity, database){
     }
   };
   
-  this.db = function(){ return database; };
 };
 
 
@@ -386,7 +399,9 @@ Graph['Entity'] = function(entity, database){
 Graph['Database'] = function(options){
   var _datasource = options && options['datasource'] ? options.datasource : {entities:[], edges:[]};
   var _cacheName = options && options['cacheName'] ? options.cacheName : 'graphDB';
+  this.indexGenerator = options ? options.indexGenerator : undefined;
   var _entities = new Graph.Collection(this);
+  // allows for custom index generation
     
   /*
   * CRUD methods
