250e12c486824a3945f6ff8a87b68c8d9ce3bc6a
jmfarp2011 <jamesmeador1983@gmail.com>
2014-12-17 04:55:25 +0000

    uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,32 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
+     pkg: grunt.file.readJSON('package.json'),
     concat: {
       options: {
         separator: ';'
       },
       dist: {
         src: ['src/graph.core.js', 'src/graph.edge.js', 'src/graph.collection.js', 'src/graph.entity.js', 'src/graph.database.js', 'src/graph.dashboard.js', 'src/graph.component.js'],
-        dest: 'graph.js'
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
       }
     }
   });
   
   grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('default', 'concat');
+  grunt.registerTask('debug', 'concat');
+  grunt.registerTask('default', ['concat', 'uglify']);
 
 };
