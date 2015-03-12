3f58c1280cb34f698ed187a097144e8ef7a49a3e
jmfarp2011 <jamesmeador1983@gmail.com>
2015-03-12 21:18:16 +0000

    added Graph.Dashboard tests

--- src/graph.dashboard.js
+++ src/graph.dashboard.js
@@ -1,104 +1,108 @@
 Graph['Dashboard'] = function(options){
-  if (!options || (!!options && !options.container)) throw new Error('No container supplied.');
-  var self = this;
-  var $container = options.container || null;
-  var layouts = {};
-  layouts['default'] = {};
-  layouts.default['filterData'] = {};
-  layouts.default['layout'] = (options && options.layout) ? options.layout : {
-      rows: [{
-          cssClass: '',
-          cols: [{
-              cssClass: '',
-              component: Graph.Component.extend({})
-          }]
-      }]
-  };
-  var db = (options && options.database) ? options.database : null;
-  
-  $container.addClass('graph_dashboard');
-  var _components = [],
-      _entity = {off:function(){}},
-      _events = [];
-      
-  var _configureLayout = function(name){
-    var layout = layouts[name].layout;
-    for (var i = 0, ilen = layout.rows.length; i < ilen; i++){
-      var $row = $('<div class="' + layout.rows[i].class + '" />').appendTo($container);
-      for (var j = 0, jlen = layout.rows[i].cols.length; j < jlen; j++){
-        $('<div class="' + layout.rows[i].cols[j].class + '" id="' + layout.rows[i].cols[j].component.name + '" />').appendTo($row);
-        self.register(layout.rows[i].cols[j].component);
-      }
-    }
-  };
-  
-  this.addLayout = function(name, filterData, layout){
-    layouts[name] = {
-      filterData: filterData,
-      layout: layout
-    };
-  };
-  
-  this.bind = function(obj){
-    //check obj type
-    if (!obj instanceof Graph.Entity) throw new TypeError('Graph.Dashboard can only be bound to objects of type Graph.Entity');
-    
-    for (var i = 0, len = _events.length; i < len; i++){
-      _entity.off(_events[i]); //debind events from old entity
-      obj.on(_events[i], this.update); //rebind events to new entity
-    }
-    _entity = obj; //replace old entity with new entity
-    
-    //clear container
-    $container.html('');
+    //check options
+    if (!options) throw new Error("No options supplied.");
+    if (!options.container) throw new Error('No options.container supplied.');
+    if (!options.database) throw new Error('No options.database supplied.');
 
-    //reconfigure layout
-    _components = [];
-    var layout = 'default';
-    for (var l in layouts){
-      var match = false;
-      var keys = Object.keys(layouts[l].filterData); 
-      len = keys.length;
-      for (i = 0; i < len; i++){
-        if (layouts[l].filterData[keys[i]] !== obj[keys[i]]){
-          match = false;
-          break;
+    //init private vars
+    var $container = options.container;
+    $container.addClass('graph_dashboard');
+    var db = options.database;
+    var layouts = {};
+    layouts['default'] = {};
+    layouts.default['filterData'] = {};
+    layouts.default['layout'] = (options && options.layout) ? options.layout : {
+        rows: [{
+            cssClass: '',
+            cols: [{
+                cssClass: '',
+                component: Graph.Component.extend({})
+            }]
+        }]
+    };
+    var _components = [],
+        _entity = {off:function(){}},
+        _events = [];
+
+    //private methods
+    var _register = function(component){
+        _components.push(component);
+        for (var e in component.events){
+            if(_events.indexOf(e) === -1)
+                _events.push(e.replace(/^(on)/m, ''));
         }
-        match = true;
-      }
-      if (!match) continue;
-      
-      layout = l;
-      break;
-    }
-    _configureLayout(layout);
+        component['dashboard'] = this;
+    };
     
-    //(re)render components
-    for (i = 0; i < _components.length; i++){
-      if (!!_components[i]['render'])
-          $('#' + _components[i].name).html(_components[i].render(_entity));
-    }
-  };
-  
-  this.bindFirst = function(filterData){
-    this.bind(db.query().filter(filterData).first());
-  };
-  
-  this.register = function(component){
-    _components.push(component);
-    for (var e in component.events){
-      if(_events.indexOf(e) === -1)
-          _events.push(e.replace(/^(on)/m, ''));
-    }
-    component['dashboard'] = this;
-  };
-  
-  this.update = function(event, data){
-    for (var i = 0; i < _components.length; i++){
-      if (_components[i].events['on'+event])
-       _components[i].events['on'+event].call(_components[i], data);
-    }
-  };
-  
-  _configureLayout('default');
+    var _configureLayout = function(name){
+        var layout = layouts[name].layout;
+        for (var i = 0, ilen = layout.rows.length; i < ilen; i++){
+            var $row = $('<div class="' + layout.rows[i].class + '" />').appendTo($container);
+            for (var j = 0, jlen = layout.rows[i].cols.length; j < jlen; j++){
+                $('<div class="' + layout.rows[i].cols[j].class + '" id="' + layout.rows[i].cols[j].component.name + '" />').appendTo($row);
+                _register(layout.rows[i].cols[j].component);
+            }
+        }
+    };
+
+    this.addLayout = function(name, filterData, layout){
+        layouts[name] = {
+            filterData: filterData,
+            layout: layout
+        };
+    };
+
+    this.bind = function(obj){
+        //check obj type
+        if (!obj instanceof Graph.Entity) throw new TypeError('Graph.Dashboard can only be bound to objects of type Graph.Entity');
+
+        for (var i = 0, len = _events.length; i < len; i++){
+            _entity.off(_events[i]); //debind events from old entity
+            obj.on(_events[i], this.update); //rebind events to new entity
+        }
+        _entity = obj; //replace old entity with new entity
+
+        //clear container
+        $container.html('');
+
+        //reconfigure layout
+        _components = [];
+        var layout = 'default';
+        for (var l in layouts){
+            var match = false;
+            var keys = Object.keys(layouts[l].filterData); 
+            len = keys.length;
+            for (i = 0; i < len; i++){
+                if (layouts[l].filterData[keys[i]] !== obj[keys[i]]){
+                    match = false;
+                    break;
+                }
+                match = true;
+            }
+            if (!match) continue;
+
+            layout = l;
+            break;
+        }
+        _configureLayout(layout);
+
+        //(re)render components
+        for (i = 0; i < _components.length; i++){
+            if (!!_components[i]['render'])
+                $('#' + _components[i].name).html(_components[i].render(_entity));
+        }
+    };
+
+    this.bindFirst = function(filterData){
+        this.bind(db.query().filter(filterData).first());
+    };
+
+    this.update = function(event, data){
+        for (var i = 0; i < _components.length; i++){
+            if (_components[i].events['on'+event])
+                _components[i].events['on'+event].call(_components[i], data);
+        }
+    };
+
+    _configureLayout('default');
 };
--- test/modules/graph.dashboard.spec.js
+++ test/modules/graph.dashboard.spec.js
@@ -1,33 +1,111 @@
 jasmine.getFixtures().fixturesPath = 'test/fixtures';
 
 describe('Graph.js Dashboard object', function(){
-    //init
-    it('should not be null or undefined', function(){
-        expect(Graph.Dashboard).not.toBe(null);
-        expect(Graph.Dashboard).toBeDefined();
-    });
-    
-    beforeEach(function(){
+
+    beforeEach(function(){        
+        var ds = {
+            entities : [
+                { name: "Tom", type: "person", age: "28", image: "http://img3.wikia.nocookie.net/__cb20120329233907/alcatraztv/images/2/22/2002_mugshot.jpg"},
+                { name: "Bob", type: "person", image: "http://images.amcnetworks.com/blogs.amctv.com/wp-content/uploads/2010/04/Krazy-8-Mugshot-760.jpg"},
+                { name: "Tom\'s house", type: "place", location: "1234 1st St"},
+                { name: "Tom\'s motorcycle", type: "thing", brand: "Honda"}
+            ], 
+            edges : [
+                { source: 'personTom', target: 'placeTom\'s house', rel: "lives at"},
+                { source: 'placeTom\'s house', target: 'personTom', rel: "residence of"},
+                { source: 'personBob', target: 'placeTom\'s house', rel: "painted"},
+                { source: 'personTom', target: 'personBob', rel: 'knows'}  
+            ]};
+        this.db = new Graph.Database({
+            datasource: ds,
+            index: function(obj){
+                return obj.type + obj.name;
+            }
+        });
+
         var c = $('<div id="container" />').appendTo($('body'));
-        this.testDashboard = new Graph.Dashboard({container: c});
+        this.testDashboard = new Graph.Dashboard({
+            container: c, 
+            database: this.db
+        });
     });
 
     afterEach(function(){
         this.testDashboard = null;
         c = null;
+        this.db = null;
     });
-    
-    it('should intialize', function(){
-        expect(this.testDashboard instanceof Graph.Dashboard).toBe(true);
-        expect($('#container').length).toBeGreaterThan(0);
-        expect($('#container')[0]).toBeInDOM();
+
+    describe('initialization', function(){
+        it('should not be null or undefined', function(){
+            expect(Graph.Dashboard).not.toBe(null);
+            expect(Graph.Dashboard).toBeDefined();
+        });
+
+        it('should be an instance of Graph.Database', function(){
+            expect(this.testDashboard instanceof Graph.Dashboard).toBe(true);            
+        });
+
+        it('should error if not passed required options(container & database)', function(){
+            expect( function(){ new Graph.Dashboard(); }).toThrow(new Error("No options supplied."));
+            expect( function(){ new Graph.Dashboard( {} ); }).toThrow(new Error("No options.container supplied."));
+
+            var c2 = $('<div id="c2" />').appendTo($('body'));
+            expect( function(){ new Graph.Dashboard({ container: c2 }); }).toThrow(new Error("No options.database supplied."));
+
+            var db = this.db;
+            expect( function(){ new Graph.Dashboard( { container: c2, database: db } ); }).not.toThrow();
+        });
+
+        it('should add "graph_dashboard" class to container', function(){
+            expect($('#container')).toHaveClass("graph_dashboard");
+        });
     });
-    
-    it('should bind to a entity', function(){
+
+    describe('binding to an entity', function(){
+        it('should bind to given entity', function(){
+            this.testDashboard.bind(this.db.query({name: 'Tom'}).first());
+            expect($('#graph_component_base_name')).toHaveClass('graph_component_base');
+            expect($('#graph_component_base_name')).toContainText('Tom');
+            expect($('#graph_component_base_type')).toContainText('person');
+            expect($('#graph_component_base_age')).toContainText('28');
+        });
+
+        it('should bind to a entity by a given filter object', function(){
+            this.testDashboard.bindFirst({name: 'Tom'});
+            expect($('#graph_component_base_name')).toHaveClass('graph_component_base');
+            expect($('#graph_component_base_name')).toContainText('Tom');
+            expect($('#graph_component_base_type')).toContainText('person');
+            expect($('#graph_component_base_age')).toContainText('28');
+        });
+    });
+
+    describe('layout management', function(){
+        it('should add a layout', function(){
+            this.testDashboard.addLayout('Person', {type: 'person'}, {
+                rows: [{
+                    cssClass: '',
+                    cols: [{
+                        cssClass: '',
+                        component: Graph.Component.extend({name: 'person'})
+                    }]
+                }]
+            });
+            this.testDashboard.bindFirst({name: 'Tom'});
+            expect($('#graph_component_person_name')).toHaveClass('graph_component_person');
+            expect($('#graph_component_person_name')).toContainText('Tom');
+            expect($('#graph_component_person_type')).toContainText('person');
+            expect($('#graph_component_person_age')).toContainText('28');
+        });
+    });
+
+    describe('event handling', function(){
+        it('should handle onchange event', function(){
+
+        });
         
-    });
-    
-    it('should add a layout', function(){
-        
+        it('should handle onaddedge event', function(){
+
+        });
     });
 });
